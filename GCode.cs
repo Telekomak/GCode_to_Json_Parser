743110257a9626991fafa44314054097f36b2587
Telekomak <65535357+Telekomak@users.noreply.github.com>
2021-06-17 13:40:43 +0200

    Update GCode.cs

--- GCode.cs
+++ GCode.cs
@@ -6,7 +6,7 @@ using System.Runtime.Serialization.Formatters.Binary;
 using System.Threading.Tasks;
 using Newtonsoft.Json.Linq;
 
-namespace GCodeManager
+namespace GCodeParser
 {
     [Serializable]
     public class GCode
